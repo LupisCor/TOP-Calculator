db84451b992f2d878f5627853a91ff89a88b9390
Lupis_ <doliver575@gmail.com>
2023-09-05 13:13:03 -0400

    Write functions for number buttons, delete button, and clear button

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
-let number1 = 4;
-let number2 = 2;
-let operator = '/';
+let number1 = NaN;
+let number2 = NaN;
+let operator = '';
+let currentInput = '';
+
+const inputText = document.querySelector('.input-text');
 
 function addition(a, b){
     return a + b;
@@ -39,9 +42,25 @@ function operate(num1, op, num2) {
     }
 }
 
+const numberButtons = document.querySelectorAll('.number-button');
+numberButtons.forEach((button) => {
+    button.onclick = function(e){
+        currentInput += e.target.innerHTML;
+        inputText.innerHTML = currentInput;
+    }
+});
 
+document.querySelector('#clear').onclick = function(){
+    currentInput = ''
+    inputText.innerHTML = currentInput;
+};
 
-console.log(operate(number1, operator, number2));
+document.querySelector('#delete').onclick = function(){
+    currentInput = currentInput.slice(0, -1);
+    inputText.innerHTML = currentInput;
+};
+
+//console.log(operate(number1, operator, number2));
 // console.log(subtraction(7, 2));
 // console.log(multiplication(3, 5));
 // console.log(division(101, 23));
