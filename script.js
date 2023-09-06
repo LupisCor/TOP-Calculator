4623479f74a98e6c3d605ca1dda66240395ebc2e
Lupis_ <doliver575@gmail.com>
2023-09-06 12:14:18 -0400

    All buttons funciton. Check logic of operations.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let operator = '';
 let currentInput = '';
 
 const inputText = document.querySelector('.input-text');
+const equationText = document.querySelector('.equation-text');
 
 function addition(a, b){
     return a + b;
@@ -50,15 +51,61 @@ numberButtons.forEach((button) => {
     }
 });
 
+const operatorButtons = document.querySelectorAll('.operator-buttons');
+operatorButtons.forEach((button) => {
+    button.onclick = function(e){
+        operator = e.target.innerHTML;
+        if (isNaN(number1)){
+            number1 = Number(currentInput);
+        }
+        else {
+            number1 = operate(number1, operator, Number(currentInput));
+        }
+        clearInput();
+        equationText.innerHTML = `${number1} ${operator}`;
+    }
+});
+
 document.querySelector('#clear').onclick = function(){
-    currentInput = ''
-    inputText.innerHTML = currentInput;
+    clearInput();
+    clearEquation();
 };
 
 document.querySelector('#delete').onclick = function(){
     currentInput = currentInput.slice(0, -1);
     inputText.innerHTML = currentInput;
 };
+
+document.querySelector('#period').onclick = function(e){
+    if (currentInput === ''){
+        currentInput += "0.";
+        inputText.innerHTML = currentInput;
+    }
+    else if (!currentInput.includes(".")){
+        currentInput += e.target.innerHTML;
+        inputText.innerHTML = currentInput;
+    }
+}
+
+document.querySelector('#equals').onclick = function(){
+    if (isNaN(number1)){
+        number1 = Number(currentInput);
+    }
+    else {
+        number1 = operate(number1, operator, Number(currentInput));
+    }
+    equationText.innerHTML = number1;
+    clearInput();
+}
+
+function clearInput(){
+    currentInput = '';
+    inputText.innerHTML = currentInput;
+}
+function clearEquation(){
+    equationText.innerHTML = '';
+    number1 = NaN;
+}
 
 //console.log(operate(number1, operator, number2));
 // console.log(subtraction(7, 2));
